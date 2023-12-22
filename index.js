d84b2102fb44d6f8a6e41953ee1aa5aeb7eb4a20
MuratOfficial <toimetm@gmail.com>
2023-12-23 02:51:16 +0600

    env change

--- index.js
+++ index.js
@@ -2,28 +2,6 @@
 const express = require("express");
 const axios = require("axios");
 const cors = require("cors");
-const admin = require("firebase-admin");
-const serviceAccount = require("tel-bot-server-firebase-adminsdk-gtwyf-5217c595d6.json");
-
-// Import the functions you need from the SDKs you need
-const { initializeApp } = require("firebase/app");
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyAbOMvUFn-ofxEAc_DXsGRCtxrHSXgHewM",
-  authDomain: "tel-bot-server.firebaseapp.com",
-  projectId: "tel-bot-server",
-  storageBucket: "tel-bot-server.appspot.com",
-  messagingSenderId: "922764117283",
-  appId: "1:922764117283:web:6df6c0f70eb835484eb896",
-  measurementId: "G-W5MG02GPL7",
-};
-
-// Initialize Firebase
-const app1 = initializeApp(firebaseConfig);
 
 const app = express();
 const port = 3001; // You can use any port you prefer
